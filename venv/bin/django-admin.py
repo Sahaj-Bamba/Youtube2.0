15f9535f58c82e4c8a7cae7cedf330277c1a268d
Sahaj-Bamba <sahajbamba1999@gmail.com>
2019-09-13 08:42:53 +0530

    Restarting everything again again . VENV copied from previous version

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sahaj-bamba/Desktop/webster/Video-Sharing-Portal/youtube/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

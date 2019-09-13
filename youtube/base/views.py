e18672bdc12a29047732676798f2b8048294fb20
Sahaj-Bamba <sahajbamba1999@gmail.com>
2019-09-13 08:49:29 +0530

    Copied base app

---
+++ youtube/base/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def home(request):
+    return render(request, 'base/home.html', {})
+
+
+def not_found(request):
+    return render(request, 'base/not_found.html', {})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def update(request):
+#     try:
+#         if request.user.is_authenticated:
+#             pass
+#             temp = User_details.objects.get(user_user=request.user)
+#             request.session['gamer_type'] = temp.type
+#             request.session['gamer_authority'] = temp.authority
+#         else :
+#             request.session['gamer_type'] = 0
+#             request.session['gamer_authority'] = 0
+#     except ObjectDoesNotExist:
+#         request.session['gamer_type'] = 0
+#         request.session['gamer_authority'] = 0
+#
